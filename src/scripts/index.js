7eb34f2c8e74609e4a3a1bed7374e32507505d9c
thenotoriousgustav <guscae4@gmail.com>
2022-11-17 23:00:47 +0700

    all done, responsive, link, etc

---
+++ src/scripts/index.js
@@ -0,0 +1,6 @@
+let nav = document.getElementById("nav"),
+  button = document.getElementById("button");
+
+button.onclick = function () {
+  nav.classList.toggle("responsive");
+};
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./src/**/*.{html,js}", "*.{html,js}"],
   theme: {
     extend: {
       backgroundImage: {
-        header: "url('/src/assets/img/img1.jpg')",
+        header: "url('/src/assets/img/img3.jpg')",
+        city: "url('/src/assets/img/city.jpg')",
+        summerhill: "url('/src/assets/img/summerhill.jpg')",
+        hive: "url('/src/assets/img/hive.jpg')",
+        canary: "url('/src/assets/img/canary.jpg')",
       },
     },
   },
